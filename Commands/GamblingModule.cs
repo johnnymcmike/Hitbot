bc620776594e97c3904b2d0a981801d157024d48
johnnymcmike <43422183+johnnymcmike@users.noreply.github.com>
2022-02-21 00:36:00 -0500

    this was dumb

--- Commands/GamblingModule.cs
+++ Commands/GamblingModule.cs
@@ -10,72 +10,65 @@ public class GamblingModule : BaseCommandModule
     public EconManager econ { get; set; }
     public Random rand { get; set; }
 
-    [Command("slots")]
+
     public override Task AfterExecutionAsync(CommandContext ctx)
     {
         econ.WriteBalances();
         return Task.Delay(0);
     }
 
+    [Command("slots")]
     public async Task SlotMachine(CommandContext ctx)
     {
-        try
+        DiscordMember? caller = ctx.Member;
+        string callerString = econ.GetBalancebookString(caller);
+        if (!econ.BalanceBook.ContainsKey(callerString) || econ.BalanceBook[callerString] < 1)
         {
-            DiscordMember? caller = ctx.Member;
-            string callerString = econ.GetBalancebookString(caller);
-            if (!econ.BalanceBook.ContainsKey(callerString) || econ.BalanceBook[callerString] < 1)
+            await ctx.Channel.SendMessageAsync("Insufficient funds.");
+            return;
+        }
+
+        var possibleemojis = new Dictionary<string, int>
+        {
+            {":1kbtroll:", -20},
+            {":cherries:", 30},
+            {":seven:", 100},
+            {":fish:", 40},
+            {":cowredeyes:", 20},
+            {":it:", 15}
+        };
+        string slotresultstr = " ";
+        DiscordMessage slotmsg = await ctx.Channel.SendMessageAsync("Spinning...");
+        await Task.Delay(2000);
+
+        string[] results = new string[3];
+        for (int i = 0; i < 3; i++)
+        {
+            string choice = possibleemojis.Keys.ToArray()[rand.Next(possibleemojis.Count)];
+            results[i] = choice;
+            await Task.Delay(i * 1000);
+            slotresultstr += DiscordEmoji.FromName(ctx.Client, choice).ToString();
+            await slotmsg.ModifyAsync(slotresultstr);
+        }
+
+        foreach (string emoji in possibleemojis.Keys)
+        {
+            if (results.Count(x => x == emoji) == 2)
             {
-                await ctx.Channel.SendMessageAsync("Insufficient funds.");
+                int reward = possibleemojis[emoji] / 2;
+                econ.BalanceBook[callerString] += reward;
+                await ctx.Channel.SendMessageAsync($"Two {emoji}s! You win {reward} {econ.Currencyname}! Yippee!");
                 return;
             }
 
-            var possibleemojis = new Dictionary<string, int>
+            if (results.Count(x => x == emoji) == 3)
             {
-                {":1kbtroll:", -20},
-                {":cherries:", 30},
-                {":seven:", 100},
-                {":fish:", 40},
-                {":cowredeyes:", 20},
-                {":it:", 15}
-            };
-            string slotresultstr = " ";
-            DiscordMessage slotmsg = await ctx.Channel.SendMessageAsync("Spinning...");
-            await Task.Delay(2000);
-
-            string[] results = new string[3];
-            for (int i = 0; i < 3; i++)
-            {
-                string choice = possibleemojis.Keys.ToArray()[rand.Next(possibleemojis.Count)];
-                results[i] = choice;
-                await Task.Delay(i * 1000);
-                slotresultstr += DiscordEmoji.FromName(ctx.Client, choice).ToString();
-                await slotmsg.ModifyAsync(slotresultstr);
+                int reward = possibleemojis[emoji];
+                econ.BalanceBook[callerString] += reward;
+                await ctx.Channel.SendMessageAsync(
+                    $"THREE {emoji}s! that's a JACKBOT baybee! {reward} {econ.Currencyname}!!!");
+                return;
             }
-
-            foreach (string emoji in possibleemojis.Keys)
-            {
-                if (results.Count(x => x == emoji) == 2)
-                {
-                    int reward = possibleemojis[emoji] / 2;
-                    econ.BalanceBook[callerString] += reward;
-                    await ctx.Channel.SendMessageAsync($"Two {emoji}s! You win {reward} {econ.Currencyname}! Yippee!");
-                    return;
-                }
-
-                if (results.Count(x => x == emoji) == 3)
-                {
-                    int reward = possibleemojis[emoji];
-                    econ.BalanceBook[callerString] += reward;
-                    await ctx.Channel.SendMessageAsync(
-                        $"THREE {emoji}s! that's a JACKBOT baybee! {reward} {econ.Currencyname}!!!");
-                    return;
-                }
-            }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-            throw;
         }
     }
 }
