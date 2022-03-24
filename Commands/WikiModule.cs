e5e04d773d92d547d0e0ceffef0db7d9eaf7be66
johnnymcmike <43422183+johnnymcmike@users.noreply.github.com>
2022-03-24 18:45:24 -0400

    done with randomwiki

--- Commands/WikiModule.cs
+++ Commands/WikiModule.cs
@@ -20,7 +20,13 @@ public class WikiModule : BaseCommandModule
         var response = await _http.GetAsync("https://en.wikipedia.org/api/rest_v1/page/random/summary");
         response.EnsureSuccessStatusCode();
         string content = await response.Content.ReadAsStringAsync();
-        string? sas = Convert.ToString(JObject.Parse(content)["content_urls"]["desktop"]["page"]);
-        Console.WriteLine(sas);
+        string? finalresponse = Convert.ToString(JObject.Parse(content)["content_urls"]?["desktop"]?["page"]);
+        if (finalresponse is null)
+        {
+            await ctx.RespondAsync("this should never happen");
+            return;
+        }
+
+        await ctx.RespondAsync(finalresponse);
     }
 }
