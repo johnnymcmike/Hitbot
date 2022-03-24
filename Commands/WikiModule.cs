3af83beef611fb3845a4cf0d8a75cef24e6c490b
johnnymcmike <43422183+johnnymcmike@users.noreply.github.com>
2022-03-24 19:55:06 -0400

    i think i did it

--- Commands/WikiModule.cs
+++ Commands/WikiModule.cs
@@ -39,6 +39,7 @@ public class WikiModule : BaseCommandModule
         response.EnsureSuccessStatusCode();
         string content = await response.Content.ReadAsStringAsync();
         string wa = content.Split('[')[4];
+        wa = wa.Substring(1);
         Console.WriteLine(wa);
         if (wa.IndexOf("\"") == -1)
         {
