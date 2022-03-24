66b49505e07afaacefc02a49d9a65619c52cfec2
johnnymcmike <43422183+johnnymcmike@users.noreply.github.com>
2022-03-24 18:26:57 -0400

    await

--- Commands/WikiModule.cs
+++ Commands/WikiModule.cs
@@ -18,7 +18,7 @@ public class WikiModule : BaseCommandModule
     {
         var response = await _http.GetAsync("https://en.wikipedia.org/api/rest_v1/page/random/summary");
         response.EnsureSuccessStatusCode();
-        var content = response.Content.ReadAsStringAsync();
+        string content = await response.Content.ReadAsStringAsync();
         Console.WriteLine(content);
     }
 }
