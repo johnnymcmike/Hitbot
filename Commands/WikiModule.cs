fd791a5f4b16580af108d8244d9df7f1d41e5fb1
johnnymcmike <43422183+johnnymcmike@users.noreply.github.com>
2022-03-24 18:43:30 -0400

    think i got it

--- Commands/WikiModule.cs
+++ Commands/WikiModule.cs
@@ -1,6 +1,6 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
-using Emzi0767.Utilities;
+using Newtonsoft.Json.Linq;
 
 namespace Hitbot.Commands;
 
@@ -19,7 +19,8 @@ public class WikiModule : BaseCommandModule
     {
         var response = await _http.GetAsync("https://en.wikipedia.org/api/rest_v1/page/random/summary");
         response.EnsureSuccessStatusCode();
-        var content = response.Content.ToDictionary();
-        foreach (var VARIABLE in content) Console.WriteLine(VARIABLE.Key);
+        string content = await response.Content.ReadAsStringAsync();
+        string? sas = Convert.ToString(JObject.Parse(content)["content_urls"]["desktop"]["page"]);
+        Console.WriteLine(sas);
     }
 }
